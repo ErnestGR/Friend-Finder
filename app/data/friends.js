5bbe9e6b77e6f52e36d25f390f83ad66346364ab
Netcore89 <ernes.rave@gmail.com>
2019-03-31 00:26:09 -0600

    lastcommit

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsList = [
+    {
+        name: "Squidward",
+        photo: "https://images-na.ssl-images-amazon.com/images/I/6101Je6svzL._SL1500_.jpg",
+        preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+    },
+
+    {
+        name: "Patrick Star",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Patrick_Star.svg/1200px-Patrick_Star.svg.png",
+        preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+    },
+
+    {
+        name: "Sponge Bob",
+        photo: "https://vignette.wikia.nocookie.net/spongebob/images/2/2a/SpongeBob_SquarePants%28copy%290.png/revision/latest?cb=20160507142128",
+        preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    },
+];
+
+module.exports = friendsList;
